fac5ea1b4a19476e040818fca59736739bf4953d
Star <star3s@126.com>
2019-04-10 13:40:50 +0800

    Update shell bg colors

--- Shell.go
+++ Shell.go
@@ -33,6 +33,7 @@ const (
 	TextMagenta
 	TextCyan
 	TextWhite
+	TextNone TextColor = 0
 )
 
 type BgColor int
@@ -73,6 +74,12 @@ func Cyan(s string, attribs ...AttribCode) string {
 func White(s string, attribs ...AttribCode) string {
 	return Color(s, TextWhite, BgNone, attribs...)
 }
+func Dim(s string) string {
+	return Color(s, TextWhite, BgNone, AttrDim)
+}
+func Italic(s string) string {
+	return Color(s, TextWhite, BgNone, AttrItalic)
+}
 
 func BBlack(s string, attribs ...AttribCode) string {
 	return Color(s, TextWhite, BgBlack, attribs...)
@@ -81,10 +88,10 @@ func BRed(s string, attribs ...AttribCode) string {
 	return Color(s, TextWhite, BgRed, attribs...)
 }
 func BGreen(s string, attribs ...AttribCode) string {
-	return Color(s, TextWhite, BgGreen, attribs...)
+	return Color(s, TextBlack, BgGreen, attribs...)
 }
 func BYellow(s string, attribs ...AttribCode) string {
-	return Color(s, TextWhite, BgYellow, attribs...)
+	return Color(s, TextBlack, BgYellow, attribs...)
 }
 func BBlue(s string, attribs ...AttribCode) string {
 	return Color(s, TextWhite, BgBlue, attribs...)
@@ -93,7 +100,7 @@ func BMagenta(s string, attribs ...AttribCode) string {
 	return Color(s, TextWhite, BgMagenta, attribs...)
 }
 func BCyan(s string, attribs ...AttribCode) string {
-	return Color(s, TextWhite, BgCyan, attribs...)
+	return Color(s, TextBlack, BgCyan, attribs...)
 }
 func BWhite(s string, attribs ...AttribCode) string {
 	return Color(s, TextBlack, BgWhite, attribs...)
@@ -108,7 +115,9 @@ func Color(s string, textColor TextColor, bgColor BgColor, attribs ...AttribCode
 	for _, attr := range attribs {
 		sets = append(sets, strconv.Itoa(int(attr)))
 	}
-	sets = append(sets, strconv.Itoa(int(textColor)))
+	if textColor != TextNone {
+		sets = append(sets, strconv.Itoa(int(textColor)))
+	}
 	if bgColor != BgNone {
 		sets = append(sets, strconv.Itoa(int(bgColor)))
 	}
