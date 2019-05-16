03b5bde295d8640785b068ddaa517d3617616dae
Star <star3s@126.com>
2019-05-16 16:18:06 +0800

    add Convert

--- Cast.go
+++ Cast.go
@@ -8,6 +8,34 @@ import (
 	"strings"
 )
 
+func ParseInt(s string) int64 {
+	if strings.IndexByte(s, '.') != -1 {
+		i, err := strconv.ParseFloat(s, 64)
+		if err == nil {
+			return int64(i)
+		}
+	}
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err == nil {
+		return i
+	}
+	return 0
+}
+
+func ParseUint(s string) uint64 {
+	if strings.IndexByte(s, '.') != -1 {
+		i, err := strconv.ParseFloat(s, 64)
+		if err == nil {
+			return uint64(i)
+		}
+	}
+	i, err := strconv.ParseUint(s, 10, 64)
+	if err == nil {
+		return i
+	}
+	return 0
+}
+
 func Int(value interface{}) int {
 	return int(Int64(value))
 }
@@ -34,15 +62,42 @@ func Int64(value interface{}) int64 {
 			return 0
 		}
 	case []byte:
-		i, err := strconv.ParseInt(string(realValue), 10, 0)
-		if err == nil {
-			return i
-		}
+		return ParseInt(string(realValue))
 	case string:
-		i, err := strconv.ParseInt(realValue, 10, 0)
-		if err == nil {
-			return i
+		return ParseInt(realValue)
+	}
+	return 0
+}
+
+func Uint(value interface{}) uint {
+	return uint(Uint64(value))
+}
+func Uint64(value interface{}) uint64 {
+	switch realValue := value.(type) {
+	case uint:
+		return uint64(realValue)
+	case uint8:
+		return uint64(realValue)
+	case uint16:
+		return uint64(realValue)
+	case uint32:
+		return uint64(realValue)
+	case uint64:
+		return uint64(realValue)
+	case float32:
+		return uint64(realValue)
+	case float64:
+		return uint64(realValue)
+	case bool:
+		if realValue {
+			return 1
+		} else {
+			return 0
 		}
+	case []byte:
+		return ParseUint(string(realValue))
+	case string:
+		return ParseUint(realValue)
 	}
 	return 0
 }
@@ -183,6 +238,22 @@ func Strings(value interface{}) []string {
 	return make([]string, 0)
 }
 
+func GetLowerName(s string) string {
+	buf := []byte(s)
+	if buf[0] >= 'A' && buf[0] <= 'Z' {
+		buf[0] += 32
+	}
+	return string(buf)
+}
+
+func GetUpperName(s string) string {
+	buf := []byte(s)
+	if buf[0] >= 'a' && buf[0] <= 'z' {
+		buf[0] -= 32
+	}
+	return string(buf)
+}
+
 func FixUpperCase(data []byte, excludesKeys []string) {
 	n := len(data)
 	types := make([]bool, 0)
@@ -299,6 +370,198 @@ func In(arr []interface{}, s interface{}) bool {
 		}
 	}
 	return false
+}
+
+//func Elem(v reflect.Value) reflect.Value {
+//	for v.Kind() == reflect.Ptr {
+//		v = v.Elem()
+//	}
+//	return v
+//}
+
+func FinalType(v reflect.Value) reflect.Type {
+	for v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	if v.Kind() == reflect.Interface {
+		return reflect.TypeOf(v.Interface())
+	} else {
+		return v.Type()
+	}
+}
+
+func RealValue(v reflect.Value) reflect.Value {
+	for v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	return v
+}
+
+func FinalValue(v reflect.Value) reflect.Value {
+	v = RealValue(v)
+	if v.Kind() == reflect.Interface {
+		return v.Elem()
+	} else {
+		return v
+	}
+}
+
+func FixNilValue(v reflect.Value) {
+	t := v.Type()
+	for t.Kind() == reflect.Ptr && v.IsNil() {
+		v.Set(reflect.New(v.Type().Elem()))
+		v = v.Elem()
+		t = t.Elem()
+	}
+	if t.Kind() == reflect.Slice && v.IsNil() {
+		v.Set(reflect.MakeSlice(v.Type(), 0, 0))
+	}
+	if t.Kind() == reflect.Map && v.IsNil() {
+		v.Set(reflect.MakeMap(v.Type()))
+	}
+}
+
+func convertMapToStruct(from, to reflect.Value) {
+	keys := from.MapKeys()
+	keyMap := map[string]*reflect.Value{}
+	for j := len(keys) - 1; j >= 0; j-- {
+		keyMap[strings.ToLower(keys[j].String())] = &keys[j]
+	}
+
+	toType := to.Type()
+	for i := toType.NumField() - 1; i >= 0; i-- {
+		f := toType.Field(i)
+		if f.Anonymous {
+			convertMapToStruct(from, to.Field(i))
+			continue
+		}
+
+		k := keyMap[strings.ToLower(f.Name)]
+		var v reflect.Value
+		if k != nil {
+			v = from.MapIndex(*k)
+		}
+
+		if v.IsValid() && !v.IsNil() {
+			r := convert(v, to.Field(i))
+			if r != nil {
+				to.Field(i).Set(*r)
+			}
+		}
+	}
+}
+
+func convertMapToMap(from, to reflect.Value) {
+	toType := to.Type()
+	keys := from.MapKeys()
+	keyNum := len(keys)
+	for i := 0; i < keyNum; i++ {
+		k := keys[i]
+		v := from.MapIndex(k)
+		keyItem := reflect.New(toType.Key()).Elem()
+		valueItem := reflect.New(toType.Elem()).Elem()
+		convert(k, keyItem)
+		convert(v, valueItem)
+		to.SetMapIndex(keyItem, valueItem)
+	}
+}
+
+func convertStructToMap(from, to reflect.Value) {
+	toType := to.Type()
+	for i := from.NumField() - 1; i >= 0; i-- {
+		k := from.Type().Field(i).Name
+		v := from.Field(i)
+		keyItem := reflect.New(toType.Key()).Elem()
+		valueItem := reflect.New(toType.Elem()).Elem()
+		convert(k, keyItem)
+		convert(v, valueItem)
+		to.SetMapIndex(keyItem, valueItem)
+	}
+}
+
+func convertSliceToSlice(from, to reflect.Value) *reflect.Value {
+	toType := to.Type()
+	fromNum := from.Len()
+	for i := 0; i < fromNum; i++ {
+		valueItem := reflect.New(toType.Elem()).Elem()
+		convert(from.Index(i), valueItem)
+		to = reflect.Append(to, valueItem)
+	}
+	return &to
+}
+
+func Convert(from, to interface{}) interface{} {
+	r := convert(from, to)
+	if r == nil {
+		return to
+	} else {
+		return r.Interface()
+	}
+}
+
+func convert(from, to interface{}) *reflect.Value {
+	var fromValue reflect.Value
+	var toValue reflect.Value
+	if v, ok := from.(reflect.Value); ok {
+		from = v.Interface()
+		fromValue = v
+	} else {
+		fromValue = reflect.ValueOf(from)
+	}
+	if v, ok := to.(reflect.Value); ok {
+		toValue = v
+	} else {
+		toValue = reflect.ValueOf(to)
+	}
+	FixNilValue(toValue)
+
+	fromValue = FinalValue(fromValue)
+	toValue = RealValue(toValue)
+	if !fromValue.IsValid() || !toValue.IsValid() {
+		return nil
+	}
+
+	fromType := FinalType(fromValue)
+	toType := toValue.Type()
+
+	switch toType.Kind() {
+	case reflect.Bool:
+		toValue.SetBool(Bool(from))
+	case reflect.Interface:
+		toValue.Set(reflect.ValueOf(from))
+	case reflect.String:
+		toValue.SetString(String(from))
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		toValue.SetInt(Int64(from))
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		toValue.SetUint(Uint64(from))
+	case reflect.Float32, reflect.Float64:
+		toValue.SetFloat(Float64(from))
+	case reflect.Slice:
+		if fromType.Kind() == reflect.Slice {
+			return convertSliceToSlice(fromValue, toValue)
+		}
+		if toType.Kind() == reflect.Slice && toType.Elem().Kind() == reflect.Uint8 {
+			toValue.SetBytes(Bytes(from))
+		}
+	case reflect.Struct:
+		switch fromType.Kind() {
+		case reflect.Map:
+			convertMapToStruct(fromValue, toValue)
+		case reflect.Struct:
+			//convertStructToStruct(fromValue, toValue)
+		}
+	case reflect.Map:
+		switch fromType.Kind() {
+		case reflect.Map:
+			convertMapToMap(fromValue, toValue)
+		case reflect.Struct:
+			convertStructToMap(fromValue, toValue)
+		}
+	default:
+		//fmt.Println(" !!!!!!2", fromType.Kind(), toType.Kind(), toType.Elem().Kind())
+	}
+	return nil
 }
 
 func Json(value interface{}) string {
