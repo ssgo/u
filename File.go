90619af16ddbd3a8c6c57744757c6687c102ae5f
Star <star3s@126.com>
2019-05-19 21:49:19 +0800

    Load&Save support yaml

--- File.go
+++ File.go
@@ -3,7 +3,7 @@ package u
 import (
 	"bufio"
 	"encoding/json"
-	"fmt"
+	"gopkg.in/yaml.v2"
 	"io"
 	"os"
 	"os/exec"
@@ -15,12 +15,10 @@ func RunCommand(command string, args ...string) ([]string, error) {
 	cmd := exec.Command(command, args...)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		fmt.Println(err)
 		return nil, err
 	}
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
-		fmt.Println(err)
 		return nil, err
 	}
 
@@ -61,6 +59,22 @@ func ReadFile(fileName string) ([]string, error) {
 	return outs, nil
 }
 
+func ReadFullFile(fileName string, maxLen uint) (string, error) {
+	fd, err := os.OpenFile(fileName, os.O_RDONLY, 0400)
+	if err != nil {
+		return "", err
+	}
+
+	buf := make([]byte, maxLen)
+	n, err := fd.Read(buf)
+	_ = fd.Close()
+	if err != nil {
+		return "", err
+	}
+
+	return string(buf[0:n]), nil
+}
+
 func FileExists(fileName string) bool {
 	fi, err := os.Stat(fileName)
 	return err == nil && fi != nil
@@ -80,6 +94,22 @@ func CheckPath(fileName string) {
 var fileLocks = map[string]*sync.Mutex{}
 
 func Load(fileName string, to interface{}) error {
+	if strings.HasSuffix(fileName, "yml") || strings.HasSuffix(fileName, "yaml") {
+		return load(fileName, true, to)
+	} else {
+		return load(fileName, false, to)
+	}
+}
+
+func LoadYaml(fileName string, to interface{}) error {
+	return load(fileName, true, to)
+}
+
+func LoadJson(fileName string, to interface{}) error {
+	return load(fileName, false, to)
+}
+
+func load(fileName string, isYaml bool, to interface{}) error {
 	if fileLocks[fileName] == nil {
 		fileLocks[fileName] = new(sync.Mutex)
 	}
@@ -88,14 +118,35 @@ func Load(fileName string, to interface{}) error {
 
 	fp, err := os.Open(fileName)
 	if err == nil {
-		decoder := json.NewDecoder(fp)
-		err = decoder.Decode(&to)
+		if isYaml {
+			decoder := yaml.NewDecoder(fp)
+			err = decoder.Decode(to)
+		} else {
+			decoder := json.NewDecoder(fp)
+			err = decoder.Decode(to)
+		}
 		_ = fp.Close()
 	}
 	return err
 }
 
 func Save(fileName string, data interface{}) error {
+	if strings.HasSuffix(fileName, "yml") || strings.HasSuffix(fileName, "yaml") {
+		return save(fileName, true, data)
+	} else {
+		return save(fileName, false, data)
+	}
+}
+
+func SaveYaml(fileName string, data interface{}) error {
+	return save(fileName, true, data)
+}
+
+func SaveJson(fileName string, data interface{}) error {
+	return save(fileName, false, data)
+}
+
+func save(fileName string, isYaml bool, data interface{}) error {
 	CheckPath(fileName)
 
 	if fileLocks[fileName] == nil {
@@ -107,7 +158,11 @@ func Save(fileName string, data interface{}) error {
 	fp, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
 	if err == nil {
 		var buf []byte
-		buf, err = json.MarshalIndent(data, "", "  ")
+		if isYaml {
+			buf, err = yaml.Marshal(data)
+		} else {
+			buf, err = json.MarshalIndent(data, "", "  ")
+		}
 		if err == nil {
 			_, err = fp.Write(buf)
 			_ = fp.Close()
