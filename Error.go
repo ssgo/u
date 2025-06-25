8df3f4012d14a9c58d3d59ee183c01fdc3c1a4d3
Star <star3s@126.com>
2025-06-25 21:09:43 +0800

    增加 Error 和 Errorf 用于跟踪调用堆栈，尤其是能解决反射调用时无法跟踪的问题

---
+++ Error.go
@@ -0,0 +1,44 @@
+package u
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+)
+
+type Err struct {
+	Message string
+	Stack   []string
+}
+
+func (e *Err) Error() string {
+	return e.Message
+}
+
+func Errorf(format string, args ...interface{}) *Err {
+	return Error(fmt.Sprintf(format, args...))
+}
+
+func Error(err string) *Err {
+	callStacks := make([]string, 0)
+	for i := 1; i < 50; i++ {
+		_, file, line, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+		if strings.Contains(file, "/go/src/") {
+			continue
+		}
+		if strings.Contains(file, "/ssgo/log") {
+			continue
+		}
+		if strings.Contains(file, "/ssgo/") {
+			file = "**" + file[strings.LastIndex(file, "/ssgo/"):]
+		}
+		callStacks = append(callStacks, fmt.Sprintf("%s:%d", file, line))
+	}
+	return &Err{
+		Message: err,
+		Stack:   callStacks,
+	}
+}
